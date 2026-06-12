77e30265d6580567e6b056c212e61f4cd517f3c7
matiasnahuelb-boop <matiasnahuel.b@gmail.com>
2026-06-12 18:28:50 -0300

    Remove singleton registration from AppServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -6,12 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        $this->app->singleton('files', function () {
-            return new \Illuminate\Filesystem\Filesystem;
-        });
-    }
-
+    public function register(): void {}
     public function boot(): void {}
 }
