8f23461171c4abc47b1912f116abb51ecd508797
matiasnahuelb-boop <matiasnahuel.b@gmail.com>
2026-06-12 17:07:55 -0300

    app/Models/User.php

---
+++ backend/app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+
+    protected $fillable = ['name', 'email', 'password', 'role'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected function casts(): array
+    {
+        return ['password' => 'hashed'];
+    }
+}
