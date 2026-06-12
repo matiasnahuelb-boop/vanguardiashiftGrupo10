0dc6e6f66bd9eac187587edb407058030535949c
matiasnahuelb-boop <matiasnahuel.b@gmail.com>
2026-06-12 17:20:09 -0300

    registro filesystem en AppServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -6,7 +6,12 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->singleton('files', function () {
+            return new \Illuminate\Filesystem\Filesystem;
+        });
+    }
 
     public function boot(): void {}
 }
